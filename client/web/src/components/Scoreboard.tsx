f2001049c175468182390eb0025f6f7d6457d028
DESKTOP-JJPM488\Marty <martijnwelker@gmail.com>
2022-08-13 17:35:32 +0200

    Add scoreboard totals

--- client/web/src/components/Scoreboard.tsx
+++ client/web/src/components/Scoreboard.tsx
@@ -20,6 +20,10 @@ export default class ScoreBoard
 			playerState,
 		} = this.props;
 
+		const totals = this.getTotals(
+			playerState,
+		);
+
 		return (
 			<table>
 				<thead>
@@ -37,14 +41,55 @@ export default class ScoreBoard
 					) => <tr key={`points-${roundIndex}`}>
 						{
 							points.map(
-								_points => <td key={`points-${roundIndex}-${_points.nickname}`}>{_points.points}</td>,
+								_points => <td
+									key={`points-${roundIndex}-${_points.nickname}`}
+									style={{
+										textAlign: 'right',
+									}}>
+
+									{_points.points}
+								</td>,
 							)
 						}
 					</tr>,
 				)}
+				<tr>
+					{totals.map(
+						(
+							total,
+							index,
+						) => <td
+							key={`total-${index}`}
+							style={{
+								textAlign: 'right',
+								fontWeight: 'bold',
+								borderTop: '1px solid black',
+							}}>
+
+							{total}
+						</td>,
+					)}
+				</tr>
 				</tbody>
 			</table>
 		);
+	}
+
+	private getTotals (
+		playerState: PlayerState,
+	): number[] {
+		const result: number[] = [];
+
+		for (let roundIndex = 0; roundIndex < playerState.pointsPerRound.length; roundIndex++) {
+			const roundScores = playerState.pointsPerRound[roundIndex];
+
+			for (let playerIndex = 0; playerIndex < roundScores.length; playerIndex++) {
+				result[playerIndex] ??= 0;
+				result[playerIndex] += roundScores[playerIndex].points;
+			}
+		}
+
+		return result;
 	}
 
 }
